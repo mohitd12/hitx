5af42e0b4454589e26435e57bcb633d96720b2cd
Mohit Das <mohitdass589@gmail.com>
2026-02-17 18:40:35 +0530

    add Suspense in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { Suspense } from 'react';
+
 import PostsPageShell from '@/features/posts/components/PostsPageShell';
 
 export default function HomePage() {
-  return <PostsPageShell />;
+  return (
+    <Suspense>
+      <PostsPageShell />
+    </Suspense>
+  );
 }
